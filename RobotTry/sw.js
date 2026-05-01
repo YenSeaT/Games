e1b5179ea3099fbc321f138501e7145a2b2142b1
Nagachethan P M <yenseeks@gmail.com>
2026-05-02 02:34:58 +0530

    Create sw.js

---
+++ RobotTry/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'trip-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+// Install event: Cache the core assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch event: Serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  // We don't cache API calls to Open-Meteo
+  if (event.request.url.includes('api.open-meteo.com')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
